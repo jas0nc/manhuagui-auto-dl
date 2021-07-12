54892b34bf85e6cd83b1556612d6cbcf17714d96
jas0nc <33709736+jas0nc@users.noreply.github.com>
2021-07-12 13:11:25 +0800

    import modified scripts

--- download.py
+++ download.py
@@ -1,10 +1,12 @@
-# Development by HSSLCreative
-# Date: 2020/5/6
+# Development by HSSLCreative & Jas0n
+# Date: 2021/4/1
 
-import os, time, re
+import requests, os, time, re
 from get import get
 from PIL import Image
-from proxy import requests_get
+from zipfile import ZipFile
+import shutil
+import pathlib
 
 def downloadCh(url, config_json=None):
     def downloadPg(url, e, m, counter):
@@ -13,24 +15,24 @@ def downloadCh(url, config_json=None):
         'accept-language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6',
         'cache-control': 'no-cache',
         'pragma': 'no-cache',
-        'referer': 'https://www.manhuagui.com/',
+        'referer': 'https://tw.manhuagui.com/',
         'sec-fetch-dest': 'image',
         'sec-fetch-mode': 'no-cors',
         'sec-fetch-site': 'cross-site',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36'}
         #單頁最大重試次數
-        for i in range(10):
+        for i in range(1):
             try:
-                res = None
-                res = requests_get(url, params={'e':e, 'm':m}, headers = h, timeout=10)
+                res = requests.get(url, params={'e':e, 'm':m}, headers = h, timeout=10)
                 res.raise_for_status()
             except:
-                print('頁面 %s 下載失敗: %s 重試中...' % (url, res.status_code if res else 'OTHER'), end='')
-                print('等待2秒...')
+                #print('頁面 %s 下載失敗: %s 重試中...' % (url, res.status_code), end='')
+                #print('等待1秒...')
                 #每次重試間隔
-                time.sleep(2)
+                #time.sleep(1)
                 continue
-            filename = str(counter) + '_' + os.path.basename(url)
+            ##filename = str(counter) + '_' + os.path.basename(url)
+            filename = os.path.basename(url)
             file = open(filename,'wb')
             for chunk in res.iter_content(100000):
                 file.write(chunk)
@@ -43,33 +45,78 @@ def downloadCh(url, config_json=None):
                 im.save(src_filename, 'jpeg')
             #轉檔結束
             return
-        print('超過重試次數 跳過此檔案')
+        #print('超過重試次數 跳過此檔案')
+        return False
     j = get(url)
     if not j:
         return False
     bname = j['bname']
     cname = j['cname']
-    chdir(os.path.join(re.sub(r'[\\/:*?"<>|]', '_', bname), 'jpg', cname))
+
+    os.chdir(pathlib.Path(__file__).parent.absolute())
+
+    def replacenum(matched):
+        value = int(matched.group('value'))
+        return " " + str('{:03d}'.format(value)) + " "
+    padded_cname = re.sub('(?P<value>\d+)',replacenum,cname)
+    padded_cname = re.sub(r'[\\/:*?"<>|]', '_', padded_cname)
+    cname = padded_cname
+
+    CBZfilename = re.sub(r'[\\/:*?"<>|]', '_', bname)+' - '+padded_cname+'.CBZ'
+    missingpage = 0
+
+    chdir(os.path.join('temp',re.sub(r'[\\/:*?"<>|]', '_', bname), 'jpg', cname))
     os.chdir(os.path.join('..', '..'))
-    if config_json:
-        with open('config.json', 'w') as config:
-            config.write(config_json)
     chdir(os.path.join('raw', cname))
     length = j['len']
-    print('下載 %s %s 中 共%s頁' % (bname, cname, length))
+    print(' '+'下載 %s 中 共%s頁' % (CBZfilename, length))
     e = j['sl']['e']
     m = j['sl']['m']
     path = j['path']
     i = 1
+
     for filename in j['files']:
         pgUrl = 'https://i.hamreus.com' + path +  filename
-        print(os.path.basename(pgUrl))
-        print('%s / %s' % (i, length), end='\r')
-        downloadPg(pgUrl, e, m, i)
-        #每頁間隔0.5秒
-        time.sleep(0.5)
+        output_filename = filename + '.jpg'
+        src_filename = os.path.join('..', '..', 'jpg', cname, output_filename)
+        if os.path.isfile(src_filename):
+            print (' '+' '+src_filename+" - File exist, skip")
+        else:
+            print(' '+' '+os.path.basename(pgUrl)+" - Downloading                                        ")
+            #每話間隔2秒
+            print('  '+'延遲1秒...                                                                        ')
+            print('  下載 %s: %s/%s' % (CBZfilename, i, length), end='\r')
+            if (downloadPg(pgUrl, e, m, i) == False):
+            	missingpage += 1
+            	print(' '+' cannot download: '+os.path.basename(pgUrl)+" - missing page +1				")
+            time.sleep(1)
         i += 1
-    os.chdir(os.path.join('..', '..', '..'))
+    os.chdir(pathlib.Path(__file__).parent.absolute())
+    chdir(os.path.join('CBZ', re.sub(r'[\\/:*?"<>|]', '_', bname)))
+
+    z = 1
+    if missingpage == 0 and length != 0:
+        # create a ZipFile object
+        zipObj = ZipFile(CBZfilename, 'w')
+        for filename in j['files']:
+            # Add file to the zip
+            output_filename = filename + '.jpg'
+            jpglocation = os.path.join('..','..','temp', re.sub(r'[\\/:*?"<>|]', '_', bname), 'jpg', cname, output_filename)
+            zipObj.write(jpglocation, str(z).zfill(3)+'.jpg')
+            z += 1
+        # close the Zip File
+        zipObj.close()
+        print (' '+CBZfilename+" - is Created									")
+    else:
+        print (' '+CBZfilename+" - download failed, "+str(missingpage)+" pages are missing				")
+
+    # remove files
+    os.chdir(pathlib.Path(__file__).parent.absolute())
+    if os.path.isfile(os.path.join('CBZ',re.sub(r'[\\/:*?"<>|]', '_', bname),CBZfilename)):
+        jpgfolder = os.path.join('temp', re.sub(r'[\\/:*?"<>|]', '_', bname), 'jpg', cname)
+        rawfolder = os.path.join('temp', re.sub(r'[\\/:*?"<>|]', '_', bname), 'raw', cname)
+        shutil.rmtree(jpgfolder, ignore_errors=True, onerror=None)
+        shutil.rmtree(rawfolder, ignore_errors=True, onerror=None)
     return True
 
 def chdir(ds):
--- main.py
+++ main.py
@@ -1,95 +1,153 @@
-# Development by HSSLCreative
-# Date: 2020/5/6
+#!/usr/local/bin/python3
+# Development by HSSLCreative & Jas0n
+# Date: 2021/4/1
 
-import re, time, bs4, lzstring
-from download import downloadCh
+import re, time, bs4, requests, lzstring, os
+from download import downloadCh, chdir
 from generate_config import generate_config
-from proxy import requests_get
+import pathlib
+import shutil
 
-check_re = r'^(https?://([a-zA-Z0-9]*\.)?manhuagui\.com/comic/)?([0-9]+)/?'
+check_re = r'^https?://([a-zA-Z0-9]*\.)?manhuagui\.com/comic/([0-9]+)/?'
 request_url = 'https://tw.manhuagui.com/comic/%s'
 host = 'https://tw.manhuagui.com'
+comic_list = 'comic_url.txt'
 
 def main():
     print('僅供學術研究交流使用，勿作為商業用途')
-    while True:
-        print('輸入URL或是ID:')
-        #格式:https://*.manhuagui.com/comic/XXXXX
-        #是否進入章節都沒關係
-        #例如https://*.manhuagui.com/comic/XXXXX/XXXXX.html也行
-        #反正要得只有id
-        #所以純ID也可以
-        url = input()
-        try:
-            checked_id = re.match(check_re, url).group(3)
-            break
-        except:
-            print('無效的網址')
-            continue
-    try:
-        res = requests_get(request_url % checked_id)
-        res.raise_for_status()
-    except:
-        print('錯誤:可能是沒網路或被ban ip?')
-        return
-    bs = bs4.BeautifulSoup(res.text, 'html.parser')
-    title = bs.select('.book-title h1')[0]
-    print('標題: %s' % title.text)
-    authors_link = bs.select('a[href^="/author"]')
-    authors = []
-    for author in authors_link:
-        authors.append(author.text)
-    authors = '、'.join(authors)
-    config_json = generate_config(title.text, authors, cid=checked_id)
-    links = bs.select('.chapter-list a')
-    if not links:
-        links = bs4.BeautifulSoup(lzstring.LZString().decompressFromBase64(bs.select('#__VIEWSTATE')[0].attrs.get('value')), 'html.parser').select('.chapter-list a')
-    links.reverse()
-    ch_list = []
-    for link in links:
-        ch_list.append([link.attrs['title'], link.attrs['href']])
-    #sorting
-    def sort_lambda(n):
-        try:
-            return float(re.search('(\d+(\.\d+)?)', n[0]).group(0))
-        except:
-            return 0
-    ch_list.sort(key=sort_lambda)
-    print('編號 對應名稱')
-    for ch_index in range(len(ch_list)):
-        ch = ch_list[ch_index]
-        print(str(ch_index).ljust(4), ch[0])
-    print('輸入上列編號(ex:輸入1-2 5-8 10 將會下載編號 1, 2, 5, 6, 7, 8, 10 的章節)')
-    choose_chs = input()
-    tmp = re.findall(r'[0-9]+\-?[0-9]*', choose_chs)
-    choose_block_list = []
-    config_writed = False
-    for block in tmp:
-        try:
-            block = block.split('-')
-            for i in range(len(block)):
-                block[i] = int(block[i])
-                if block[i] > len(ch_list) or block[i] < 0:
-                    raise Exception('out of range')
-            if len(block) >= 2:
-                if block[1] < block[0]:
-                    block[0], block[1] = block[1], block[0]
-                choose_block_list.append([block[0], block[1]])
-            else:
-                choose_block_list.append([block[0], block[0]])
-        except:
-            continue
-    for area in choose_block_list:
-        block = ch_list[area[0]:area[1]+1]
-        for ch in block:
-            if not config_writed:
-                downloadCh(host + ch[1], config_json)
-            else:
-                downloadCh(host + ch[1])
-                config_writed = True
-            print('延遲5秒...')
-            #每話間隔5秒
-            time.sleep(5)
+    ##while True:
+    ##    print('輸入URL:')
+    ##    #格式:https://*.manhuagui.com/comic/XXXXX
+    ##    #是否進入章節都沒關係
+    ##    #例如https://*.manhuagui.com/comic/XXXXX/XXXXX.html也行
+    ##    #反正要得只有id
+    ##    url = input()
+    print('workdir: '+str(pathlib.Path(__file__).parent.absolute()))
+    os.chdir(pathlib.Path(__file__).parent.absolute())
+    with open(comic_list) as my_comic_url:
+        my_comic = list(my_comic_url)
+        my_comic_url.close()
+        new_comic = my_comic
+        for line in my_comic:
+            os.chdir(pathlib.Path(__file__).parent.absolute())
+            url = line
+            while True:
+                try:
+                    checked_id = re.match(check_re, url).group(2)
+                    break
+                except:
+                    print('無效的網址')
+                    continue
+            try:
+                res = requests.get(request_url % checked_id)
+                res.raise_for_status()
+            except:
+                print('錯誤:可能是沒網路或被ban ip?')
+                return
+            bs = bs4.BeautifulSoup(res.text, 'html.parser')
+            title = bs.select('.book-title h1')[0]
+            print('標題: %s - %s' % (title.text, url) )
+            authors_link = bs.select('a[href^="/author"]')
+            authors = []
+            for author in authors_link:
+                authors.append(author.text)
+            authors = '、'.join(authors)
+            coverlink = bs.select('.hcover img')
+            coverlink = [x['src'] for x in coverlink][0]
+            #print(coverlink)
+            description = bs.select('div#intro-cut')[0]
+            #print(description)
+            status = bs.select('.status span')[0]
+            #print(status)
+            config_json = generate_config(title.text, authors, coverlink, url, description.text, status.text)
+            links = bs.select('.chapter-list a')
+            if not links:
+                links = bs4.BeautifulSoup(lzstring.LZString().decompressFromBase64(bs.select('#__VIEWSTATE')[0].attrs.get('value')), 'html.parser').select('.chapter-list a')
+            links.reverse()
+            ch_list = []
+            for link in links:
+                ch_list.append([link.attrs['title'], link.attrs['href']])
+            ##print('編號 對應名稱')
+            ##for ch_index in range(len(ch_list)):
+            ##    ch = ch_list[ch_index]
+            ##    print(str(ch_index).ljust(4), ch[0])
+            ##print('輸入上列編號(ex:輸入1-2 5-8 10 將會下載編號 1, 2, 5, 6, 7, 8, 10 的章節)')
+            ##choose_chs = input()
+            ##tmp = re.findall(r'[0-9]+\-?[0-9]*', choose_chs)
+            ##choose_block_list = []
+            config_writed = False
+            ##for block in tmp:
+            ##    try:
+            ##        block = block.split('-')
+            ##        for i in range(len(block)):
+            ##            block[i] = int(block[i])
+            ##            if block[i] > len(ch_list) or block[i] < 0:
+            ##                raise Exception('out of range')
+            ##        if len(block) >= 2:
+            ##            if block[1] < block[0]:
+            ##                block[0], block[1] = block[1], block[0]
+            ##            choose_block_list.append([block[0], block[1]])
+            ##        else:
+            ##            choose_block_list.append([block[0], block[0]])
+            ##    except:
+            ##        continue
+            ##for area in choose_block_list:
+            ##    block = ch_list[area[0]:area[1]+1]
+            ##    for ch in block:
+            allchdownloaded = True
+            for ch in ch_list:
+                bname = title.text
+                cname = ch[0]
+                def replacenum(matched):
+                    value = int(matched.group('value'))
+                    return " " + str('{:03d}'.format(value)) + " "
+                padded_cname = re.sub('(?P<value>\d+)',replacenum,cname)
+                padded_cname = re.sub(r'[\\/:*?"<>|]', '_', padded_cname)
+                cname = padded_cname
+
+                CBZfilename = re.sub(r'[\\/:*?"<>|]', '_', bname)+' - '+padded_cname+'.CBZ'
+                os.chdir(pathlib.Path(__file__).parent.absolute())
+                chdir(os.path.join('CBZ', re.sub(r'[\\/:*?"<>|]', '_', bname)))
+                if config_json:
+                    if not os.path.isfile('config.json'):
+                        with open(os.path.join('config.json'), 'w', encoding='utf-8') as config:
+                            config.write(config_json)
+                            print (' '+'config_json'+" - is created")
+                if not os.path.isfile('cover.jpg'):
+                    response = requests.get(coverlink)
+                    file = open("cover.jpg", "wb")
+                    file.write(response.content)
+                    file.close()
+                    print (' '+'cover.jpg'+" - is created")
+                if os.path.isfile(CBZfilename):
+                    print (' '+CBZfilename+" - File exist, skip")
+                else:
+                    allchdownloaded = False
+                    if not config_writed:
+                        downloadCh(host + ch[1], config_json)
+                    else:
+                        downloadCh(host + ch[1])
+                        config_writed = True
+                    #每話間隔2秒
+                    print(' '+'延遲2秒...')
+                    time.sleep(2)
+            if allchdownloaded:
+                bname = title.text
+                # remove temp folder
+                os.chdir(pathlib.Path(__file__).parent.absolute())
+                tempfolder = os.path.join('temp', re.sub(r'[\\/:*?"<>|]', '_', bname))
+                if os.path.isdir(tempfolder):
+                    shutil.rmtree(tempfolder)
+                print (bname+" - All chapters are downloaded, next")
+                #每漫畫間隔2秒
+                print('延遲2秒...')
+                time.sleep(2)
+                #move complete series to the end of url file
+                new_comic.append(new_comic.pop(new_comic.index(line)))
+                os.chdir(pathlib.Path(__file__).parent.absolute())
+                with open(comic_list, 'w') as f:
+                    for item in new_comic:
+                        f.write("%s" % item)
 main()
 
 #各話間會延遲5秒 各頁間會延遲1秒
